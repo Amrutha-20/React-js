54ac4ec436735da482667086614c852600a50e30
Amrutha A <amruthaammu2012005@gmail.com>
2023-11-19 19:27:51 +0530

    comment

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Day2c1 from './Classexercise/Day2c1';
+import Day2c2 from './Classexercise/Day2c2';
+import BasicForm from './Classexercise/basic';
+
+function App() {
+  return (
+    <div className="App">
+      <BasicForm/>
+      <Day2c1/>
+      <Day2c2/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Classexercise/Day2c2.js
@@ -0,0 +1,12 @@
+//hello
+import React, { Component } from 'react'
+
+export default class Day2c2 extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    )
+  }
+}
